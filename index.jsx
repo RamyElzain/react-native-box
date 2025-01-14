b13c5e456f1468219af63b79b144b685e7a431d3
RamyElzain <ramyelzain18@gmail.com>
2025-01-14 15:20:35 +0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {Text,View,StyleSheet,ScrollView} from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <ScrollView contentContainerStyle={styles.container}horizontal 
+      showsHorizontalScrollIndicator={false}>
+      <View style={styles.box}>
+        <Text style={styles.text} >1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text} >2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text} >3</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text} >4</Text>
+      </View>
+
+      </ScrollView>
+    <View style={styles.heater}></View>
+
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container:{
+    flexDirection:'row',
+    gap:40,
+  },
+  box:{
+    height:100,
+    width:100,
+    backgroundColor:'darkblue',
+    margin:'auto',
+    marginTop:40, 
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:100,
+  },
+
+  text:{
+    color:'white',
+    fontSize:20,
+    fontWeight:'bold', 
+  },
+  header:{
+    height:100,
+    width:'100%',
+    backgroundColor:'red',
+    marginTop:40, 
+
+  }
+   
+  
+  
+})
+
+export default App;
